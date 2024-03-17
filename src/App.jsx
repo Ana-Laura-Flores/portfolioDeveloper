486a44abf4b5a4d1b9c2dadf2a20016b96dd8d59
Ana Laura Flores <anadisenio@hotmail.com>
2024-03-17 11:31:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import  logoKrear  from './assets/image/logo_krear.png'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <a href="https://www.linkedin.com/in/kreardisenio/" target="_blank">
+          <img src={logoKrear} className="logo" alt="krear logo" />
+        </a>
+        
+      </div>
+      <h1>En construcción portfolio developer ana laura</h1>
+     
+    </>
+  )
+}
+
+export default App
